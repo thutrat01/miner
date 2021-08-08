90971827764bd991fd7caba787624728f4296b1f
thutrat01 <88629080+thutrat01@users.noreply.github.com>
2021-08-08 18:18:20 +0000

    Update and rename xmr.sh to veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,7 +1,8 @@
-#xmr
+#veruscoin
 
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz 
 tar xf xmrig-6.13.1-linux-x64.tar.gz
 
+cd xmrig-6.13.1
 
 ./xmrig -o pool.supportxmr.com:443 -u 46b3zfEXZDD4nAcQ6jdYbpLRcQAeD3XWo9xTaEqtHF1tJ8URjjpAeiu6JtAaWwZ1GTRq8JAkPFp4cUbkempBKLo9EGah44r -k --tls -p 12 --cpu 4
